c197f76c1dec95b40bf7c0813624ca5b3d9e37f7
M Fazeel <mujaddadfazeel@gmail.com>
2023-06-26 12:51:11 -0500

    removed client.js

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const URL = "https://ltsuerhgiwrvsjfwjgax.supabase.co"
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0c3VlcmhnaXdydnNqZndqZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcxMDgwNDIsImV4cCI6MjAwMjY4NDA0Mn0.QVlRTKv2mMMxSjkkApfqPaEuiydcsOiB6p5hpVTa_rQ"
-
-export const supabase = createClient(URL, API_KEY);
